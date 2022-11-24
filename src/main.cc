95b9317c8c85ed99e07816ebfe687ed4f7cd40d4
Lijing <lij80@163.com>
2022-11-24 10:12:48 +0800

    add dbus test

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,27 @@
+#include <iostream>
+#include <base/at_exit.h>
+#include <base/command_line.h>
+#include <base/message_loop/message_pump_type.h>
+#include <base/test/test_timeouts.h>
+#include <base/time/time.h>
+#include <dbus/bus.h>
+#include <dbus/test_service.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
+    base::AtExitManager exit_manager;
+    base::CommandLine::Init(argc, argv);
+//    TestTimeouts::Initialize();
 
-    return 0;
+    base::Thread dbus_thread("D-Bus Thread");
+    base::Thread::Options thread_options;
+    thread_options.message_pump_type = base::MessagePumpType::IO;
+    CHECK(dbus_thread.StartWithOptions(std::move(thread_options)));
+
+    dbus::TestService::Options options;
+    options.dbus_task_runner = dbus_thread.task_runner();
+    dbus::TestService* test_service = new dbus::TestService(options);
+    CHECK(test_service->StartService());
+    test_service->WaitUntilServiceIsStarted();
+    CHECK(test_service->HasDBusThread());
+    std::cout << "exit" << std::endl;
 }
